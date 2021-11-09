f6bbcd7455a16516aa472ce200676f06e26f7427
Vince Skahan <vinceskahan@gmail.com>
2021-11-09 11:16:14 -0800

    script the firstboot somewhat

---
+++ flash-pi.sh
@@ -0,0 +1,17 @@
+
+host=pi4jr
+
+GIT_REPO_PATH= ~/desktop/github/home-pi-rebuild
+
+cd ${GIT_REPO_PATH} 2>/dev/null
+if [ x${?} != "x0" ]
+then
+  echo "oops - ${GIT_REPO_PATH} not found"
+  exit 1
+fi
+
+export HOST_NAME_HERE="${host}"
+ssh-copy-id -i ~/.ssh/id_rsa_nopass pi@${HOST_NAME_HERE}
+
+ansible-playbook site.yml -i hosts -l ${HOST_NAME_HERE} --extra-vars='firstboot=True'
+
