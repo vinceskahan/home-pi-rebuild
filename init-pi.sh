4de6328b617c5848c3de57095b9f43fcdfc3289f
Vince Skahan <vinceskahan@gmail.com>
2021-11-11 15:07:15 -0800

    rename file

--- init-pi.sh
+++ init-pi.sh
@@ -1,7 +1,7 @@
 
 host=pi4jr
 
-GIT_REPO_PATH= ~/desktop/github/home-pi-rebuild
+GIT_REPO_PATH=/Users/vince/desktop/github/home-pi-rebuild 
 
 cd ${GIT_REPO_PATH} 2>/dev/null
 if [ x${?} != "x0" ]
@@ -13,5 +13,5 @@ fi
 export HOST_NAME_HERE="${host}"
 ssh-copy-id -i ~/.ssh/id_rsa_nopass pi@${HOST_NAME_HERE}
 
-ansible-playbook site.yml -i hosts -l ${HOST_NAME_HERE} --extra-vars='firstboot=True'
+ansible-playbook ${GIT_REPO_PATH}/site.yml -i hosts -l ${HOST_NAME_HERE} --extra-vars='firstboot=True'
 
