ec433fdd686101f8bf143692dbb266e59bda855b
Vince Skahan <vinceskahan@gmail.com>
2024-12-04 17:24:50 -0800

    add prompt for y/n init, update hosts

--- init-pi.sh
+++ init-pi.sh
@@ -1,5 +1,5 @@
 
-host=pi3plus
+#host=pi3plus
 #host=pi4jr
 # host=zero
 #host=pi3jr-wifi
@@ -7,7 +7,7 @@ host=pi3plus
 #host=pi3
 #host=pi3jr-wifi
 #host=zerow2
-#host=pi5
+host=pi5
 
 GIT_REPO_PATH=/Users/vince/Desktop/gickup-backups/github.com/vinceskahan/home-pi-rebuild
 
@@ -19,7 +19,20 @@ then
 fi
 
 export HOST_NAME_HERE="${host}"
-ssh-copy-id -i ~/.ssh/id_rsa_nopass pi@${HOST_NAME_HERE}
 
-ansible-playbook -i hosts -l ${HOST_NAME_HERE} --extra-vars='firstboot=True' ${GIT_REPO_PATH}/site.yml
+# ask for confirmation
+read -p "initialize ${HOST_NAME_HERE} [y/n]: " -n 1 -r
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+  echo ""
+  echo "initializing host ${HOST_NAME_HERE}....................."
+  ssh-copy-id -i ~/.ssh/id_rsa_nopass pi@${HOST_NAME_HERE}
+  ansible-playbook -i hosts -l ${HOST_NAME_HERE} --extra-vars='firstboot=True' ${GIT_REPO_PATH}/site.yml
+else
+  echo ""
+  echo "not updating ${HOST_NAME_HERE} - exiting"
+  exit 0 
+fi
+
+exit 0
 
